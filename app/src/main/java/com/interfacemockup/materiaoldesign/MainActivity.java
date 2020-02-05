690fefea3be47d47a5c593bcc7159c171c4d24c9
Zeljko Bilbija <bilbijazeljko@gmail.com>
2020-02-05 07:26:04 +0100

    Toas added

--- app/src/main/java/com/interfacemockup/materiaoldesign/MainActivity.java
+++ app/src/main/java/com/interfacemockup/materiaoldesign/MainActivity.java
@@ -3,12 +3,55 @@ package com.interfacemockup.materiaoldesign;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Button okBtn = (Button)findViewById(R.id.buttonOK);
+    private Button okBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setListeneronOKBtn();
     }
+
+
+
+    public void setListeneronOKBtn(){
+        okBtn = (Button) findViewById(R.id.buttonOK);
+
+        okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("OK Button pressed");
+                //Toast.makeText(getApplicationContext(),"Hello Javatpoint",Toast.LENGTH_SHORT).show();
+
+
+
+                //Creating the LayoutInflater instance
+                LayoutInflater li = getLayoutInflater();
+                //Getting the View object as defined in the customtoast.xml file
+                View layout = li.inflate(R.layout.customtoast,(ViewGroup) findViewById(R.id.custom_toast_layout));
+
+                //Creating the Toast object
+                Toast toast = new Toast(getApplicationContext());
+                toast.setDuration(Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.CENTER_VERTICAL, 0, 450);
+                toast.setView(layout);//setting the view of custom toast layout
+                toast.show();
+
+            }
+        });
+    }
+
+
 }
